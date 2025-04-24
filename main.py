8fd6db33a709d23ec7a41bb50f967916d1a4406c
Eleanor Wright <elliewright055@gmail.com>
2025-04-24 14:24:12 -0500

    Create get_random_pokemon_data()

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ import datetime
 import requests
 import random
 
+def get_random_pokemon_data():  
+    pokemon_list_url = "https://pokeapi.co/api/v2/pokemon-species"
+    pokemon_list_response = requests.get(pokemon_list_url)
+    pokemon_count = pokemon_list_response.json()["count"]
+
+    url = f"https://pokeapi.co/api/v2/pokemon-species/{random.randint(1, pokemon_count)}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        pokemon = response.json()
+        pokemon_name = pokemon["name"]
+        french_name = pokemon["names"][4]["name"]
+        print(f"Today's random Pokemon fact is {pokemon_name}, or {french_name} in French.")
+    else:
+        print("There was an error loading Pokemon data.")
+
 def main():
     current = str(datetime.date.today())
     current_month = current[5:7]
@@ -10,25 +26,14 @@ def main():
     trivia_url = f"http://numbersapi.com/{current_month}/{current_day}"
     trivia_response = requests.get(trivia_url)
 
-    poke_url = "https://pokeapi.co/api/v2/pokemon-species"
-    poke_response = requests.get(poke_url)
-
-    if trivia_response.status_code == 200 and poke_response.status_code == 200:
+    if trivia_response.status_code == 200:
         trivia_fact = trivia_response.text
-        pokemon_count = poke_response.json()["count"]
-
-        random_pokemon_url = f"https://pokeapi.co/api/v2/pokemon-species/{random.randint(1, pokemon_count)}"
-        random_pokemon_response = requests.get(random_pokemon_url)
 
         print(f"Good morning! Today's date is {current}.")
         print(trivia_fact)
-
-        if random_pokemon_response.status_code == 200:
-            random_pokemon = random_pokemon_response.json()
-            random_pokemon_name = random_pokemon["name"]
-            french_translation = random_pokemon["names"][4]["name"]
-            print(f"Today's random Pokemon fact is {random_pokemon_name}, or {french_translation} in French.")
     else:
         print("There was an error. Please try again.")
+
+    get_random_pokemon_data()
 
 main()
