8a5d764ca4a5c541c8af15f26a731b03164b017a
Eleanor Wright <elliewright055@gmail.com>
2025-04-25 12:49:33 -0500

    Create format_month()

--- main.py
+++ main.py
@@ -2,6 +2,35 @@ import datetime
 import requests
 import random
 
+def format_month(month):
+    match month:
+        case "01":
+            month = "January"
+        case "02":
+            month = "February"
+        case "03":
+            month = "March"
+        case "04":
+            month = "April"
+        case "05":
+            month = "May"
+        case "06":
+            month = "June"
+        case "07":
+            month = "July"
+        case "08":
+            month = "August"
+        case "09":
+            month = "September"
+        case "10":
+            month = "October"
+        case "11":
+            month = "November"
+        case "12":
+            month = "December"
+
+    return month
+
 def get_random_pokemon_data():  
     pokemon_list_url = "https://pokeapi.co/api/v2/pokemon-species"
     pokemon_list_response = requests.get(pokemon_list_url)
@@ -20,8 +49,11 @@ def get_random_pokemon_data():
 
 def get_trivia_data():
     current = str(datetime.date.today())
+    current_year = current[0:4]
     current_month = current[5:7]
     current_day = current[8:]
+
+    formatted_month = format_month(current_month)
 
     trivia_url = f"http://numbersapi.com/{current_month}/{current_day}"
     trivia_response = requests.get(trivia_url)
@@ -29,7 +61,7 @@ def get_trivia_data():
     if trivia_response.status_code == 200:
         trivia_fact = trivia_response.text
 
-        print(f"Good morning! Today's date is {current}.")
+        print(f"Good morning! Today's date is {formatted_month} {current_day}, {current_year}.")
         print(trivia_fact)
     else:
         print("There was an error. Please try again.")
